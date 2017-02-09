9ad23498939a20d50f7121f766247b6935186bb5
lesserc <cdlesser@provide.net>
2017-02-08 21:29:53 -0500

    Fixed turn while moving backwards issue

--- frc_grizzlies_2017_steamworks/src/org/usfirst/frc/team66/robot/Constants.java
+++ frc_grizzlies_2017_steamworks/src/org/usfirst/frc/team66/robot/Constants.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team66.robot;
 
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
@@ -24,6 +25,9 @@ public class Constants {
 	public static final int BACK_BUTTON   = 7;
 	public static final int A_BUTTON      = 1;
 	
+	public static final double DEAD_ZONE_LIMIT = 0.3;
+	public static final double TRIGGER_ACTIVE_THRESHOLD = 0.5;
+	
 	//DIO	
 	
 	//Drivetrain Config
@@ -32,8 +36,17 @@ public class Constants {
 	public static final CANTalon RIGHT_MASTER_MOTOR = new CANTalon(2);
 	public static final CANTalon RIGHT_SLAVE_MOTOR  = new CANTalon(3);
 	
-	public static final boolean LEFT_DRIVE_REVERSED   = false;
-	public static final boolean RIGHT_DRIVE_REVERSED  = true;
+	public static final boolean LEFT_DRIVE_REVERSED   = true;
+	public static final boolean RIGHT_DRIVE_REVERSED  = false;
+	
+	public static final double MIN_TALON_RAMP_RATE = 1.173;//This is the min ramp rate per CTRE documentation
+	public static final boolean RAMP_ENABLED       = true;
+	public static final double DRIVE_RAMP_TIME     = 0.25;//This is the time to ramp from 0V to +/-12V in sec
+	public static final double DRIVE_RAMP_RATE     = (RAMP_ENABLED ? (Math.max(MIN_TALON_RAMP_RATE, 
+													 	(12/DRIVE_RAMP_TIME))) : 0.0);
+	
+	public static final double FINESSE_GAIN = 0.6;
+	public static final double TURN_GAIN    = 1.0;
 	
 	//Encoder Config
 	public static final int LEFT_ENCODER_CHANNEL_A  = 0;
@@ -44,7 +57,7 @@ public class Constants {
 	public static final boolean LEFT_ENCODER_REVERSED  = true;
 	public static final boolean RIGHT_ENCODER_REVERSED = false;
 	
-	public static final double ENCODER_PULSES_PER_REV     = 360;
+	public static final double ENCODER_PULSES_PER_REV     = 360; //This comes from the datasheet.
 	public static final double WHEEL_DIAMETER             = 6.0; //diameter of wheels in inches. Practice bot 6", Comp bot
 	public static final double DRIVETRAIN_MULTIPLIER      = 5.4; // Ratio of encoder revs to wheel revs, 54:30 x 36:12
 	public static final double ENCODER_DISTANCE_PER_PULSE = (WHEEL_DIAMETER*PI)/(ENCODER_PULSES_PER_REV*DRIVETRAIN_MULTIPLIER);
@@ -57,26 +70,10 @@ public class Constants {
 			                                                      RIGHT_ENCODER_CHANNEL_B,
 			                                                      RIGHT_ENCODER_REVERSED,
 			                                                      CounterBase.EncodingType.k4X);
-	
-	//Drivetrain Constants
-	public static final double LEFT_MOTOR_SCALER = -1.0;
-	public static final double RIGHT_MOTOR_SCALER = 1.0;
-	
-	public static final double MIN_TALON_RAMP_RATE = 1.173;//This is the min ramp rate per CTRE documentation
-	
-	public static final boolean RAMP_ENABLED       = true;
-	public static final double DRIVE_RAMP_TIME     = 0.25;//This is the time to ramp from 0V to +/-12V in sec
-	public static final double DRIVE_RAMP_RATE     = (RAMP_ENABLED ? (Math.max(MIN_TALON_RAMP_RATE, 
-													 	(12/DRIVE_RAMP_TIME))) : 0.0);
-	
-	public static final double DEAD_ZONE_LIMIT = 0.3;
-	public static final double TRIGGER_ACTIVE_THRESHOLD = 0.5;
-	
-	//Scalers
-	public static final double FINESSE_GAIN = 0.6;
-	public static final double TURN_GAIN    = 1.5;
 	 
 	//Solenoids
 	public final static Solenoid SHIFT_SOLENOID = new Solenoid(0);
 	
+	//Gyro
+	public final static ADXRS450_Gyro GYRO = new ADXRS450_Gyro();
 }
